f966f27cd8ba0c4d66eca936fa5612cf54075db7
Christianmagni97 <christianmagni28@gmail.com>
2024-06-26 14:29:15 +0200

    stampa un numero in snack 4

--- snack4/index4.php
+++ snack4/index4.php
@@ -1,11 +1,14 @@
 <?php
-$array = array();
+$arrayNumber = array();
 for($i = 0; $i < 15; $i++){
    $controllo = true;
    while($controllo){
-      $numero = rand(0,100);
-      if(!in_array($numero, $array)){
+    $numero = rand(0,100);
+      if(!in_array($numero, $arrayNumber)){
          $controllo = false;
       }
    }
 }
+
+echo $numero;
+echo $controllo;
